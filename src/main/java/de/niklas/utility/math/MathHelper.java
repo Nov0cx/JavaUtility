2189d480e54b32bc3a13b751029acc7386655323
Nov0cx <niklas.leyraf44@gmail.com>
2022-05-13 07:13:53 +0200

    Math

--- src/main/java/de/niklas/utility/math/MathHelper.java
+++ src/main/java/de/niklas/utility/math/MathHelper.java
@@ -3,7 +3,9 @@ package de.niklas.utility.math;
 import de.niklas.utility.misc.Pair;
 import lombok.experimental.UtilityClass;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @UtilityClass
 public class MathHelper {
@@ -36,5 +38,39 @@ public class MathHelper {
         return nearest.getB().doubleValue();
     }
 
+    public double getGCD(double a, double b) {
+        if (b == 0) return a;
 
+        return getGCD(b, a % b);
+    }
+
+    public double getGCDIterative(double a, double b) {
+        while (b != 0) {
+            double h = a % b;
+            a = b;
+            b = h;
+        }
+
+        return a;
+    }
+
+    public List<Number> getOutliers(Collection<Number> list) {
+        double avg = getAvg(list);
+        return removeOutliers(list, avg / 2, avg);
+    }
+
+    public List<Number> removeOutliers(Collection<Number> list, double flag) {
+        return removeOutliers(list, flag, getAvg(list));
+    }
+
+    public List<Number> removeOutliers(Collection<Number> list, double flag, double avg) {
+        List<Number> outliers = new ArrayList<>();
+
+        for (Number n : list) {
+            double d = getDelta(avg, n.doubleValue());
+            if (d > flag) outliers.add(n);
+        }
+
+        return outliers;
+    }
 }
